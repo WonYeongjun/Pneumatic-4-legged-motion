55ae99104c6ff086c0cae6c822e793447ec8ea81
권준형 <165387277+joonh02@users.noreply.github.com>
2025-08-19 13:15:07 +0900

    바뀐 회로에 맞게 수정하고 모든 펌프 바로 끄는 입력(Off) 추가

--- LegControl0808.ino
+++ LegControl0808.ino
@@ -5,11 +5,9 @@ class Pump {
 public:
   int pin;  // 펌프 핀 번호
   int tmp;  // on = 1, off =0
-  int speed;
-  Pump(int pin_,int speed_) {
+  Pump(int pin_) {
     pin = pin_;
     tmp = 0;
-    speed = speed_;
   };
 
   void Initialize() {
@@ -25,7 +23,7 @@ public:
   void PumpOn() {  //pump on
     if (GetTmp() == 0) {
       tmp = 1;
-      analogWrite(pin, speed);
+      analogWrite(pin, 255);
       Serial.println("Pump On");
     } else {
       Serial.println("Already On");
@@ -50,6 +48,12 @@ public:
       PumpOff();
     }
   }
+
+  void TestOff() { // pump shutdown
+    analogWrite(pin, 0);
+    // Serial.println("Pump Shutdown");
+    tmp = 0;
+  }
 };
 
 
@@ -71,7 +75,6 @@ public:
   }
 
   void Extension(int n) {
-
     for (int i = 0; i < 130 / n; i++) {
       if (angle == 130) {
         Serial.println("already extended");
@@ -102,19 +105,14 @@ public:
   MyServo sv1, sv2, sv3;
   Pump pump1, pump2, pump3;
   int unit_angle = 10;
-  Leg(int s_pin1, int s_pin2, int s_pin3, int p_pin1, int p_pin2, int p_pin3, int speed1, int speed2, int speed3)
-    : sv1(s_pin1), sv2(s_pin2), sv3(s_pin3), pump1(p_pin1,speed1), pump2(p_pin2,speed2), pump3(p_pin3,speed3) {
+  Leg(int s_pin1, int s_pin2, int s_pin3, int p_pin1, int p_pin2, int p_pin3)
+    : sv1(s_pin1), sv2(s_pin2), sv3(s_pin3), pump1(p_pin1), pump2(p_pin2), pump3(p_pin3) {
   }
 
   void Leg_PumpOn() {
     pump1.PumpOn();
     pump2.PumpOn();
     pump3.PumpOn();
-  }
-  void Leg_PumpOff() {
-    pump1.PumpOff();
-    pump2.PumpOff();
-    pump3.PumpOff();
   }
 
   void Initialize() {
@@ -131,13 +129,11 @@ public:
     Leg_PumpOn();
     Serial.println("Bend to 12");
 
-
     sv3.attach(sv3.pin);  // Extension 먼저
     sv3.Extension(unit_angle);
     delay(150);
     sv3.detach();
 
-    
     sv1.attach(sv1.pin);
     sv1.Contraction(unit_angle);
     delay(150);
@@ -146,19 +142,13 @@ public:
     sv2.attach(sv2.pin);
     sv2.Contraction(unit_angle);
     delay(150);
-    sv2.detach();
-    // Leg_PumpOn();
-
-    
+    sv2.detach();    
   }
 
-
-
-
-  void Bending23() {
+  void Bending23() 
     Leg_PumpOn();
-
     Serial.println("Bend to 23");
+
     sv1.attach(sv1.pin);
     sv1.Extension(unit_angle);
     delay(150);
@@ -169,12 +159,10 @@ public:
     delay(150);
     sv2.detach();
 
-
     sv3.attach(sv3.pin);  // 2번 펌프 수축
     sv3.Contraction(unit_angle);
     delay(150);  // 3번 펌프 팽창
     sv3.detach();
-    // Leg_PumpOn();
   }
 
   void Bending31() {  // 2, 3번 튜브 방향으로 굽어짐.
@@ -191,20 +179,15 @@ public:
     delay(150);
     sv1.detach();
 
-    
-
-
     sv3.attach(sv3.pin);  // 2번 펌프 수축
     sv3.Contraction(unit_angle);
     delay(150);  // 3번 펌프 팽창
     sv3.detach();
-    // Leg_PumpOn();
   }
 
 
   void Extension() {  // 모든 튜브 팽창
-    Leg_PumpOff();
-
+    Leg_PumpOn();
     Serial.println("팽창");
 
     sv1.attach(sv1.pin);
@@ -212,42 +195,35 @@ public:
     delay(150);
     sv1.detach();
 
-
     sv2.attach(sv2.pin);
     sv2.Extension(unit_angle);
     delay(150);
     sv2.detach();
 
-
     sv3.attach(sv3.pin);
     sv3.Extension(unit_angle);
     delay(150);
     sv3.detach();
-    Leg_PumpOn();
   }
 
   void Contraction() {
-    Leg_PumpOff();
+    Leg_PumpOn();
+    Serial.println("수축");
+
     sv1.attach(sv1.pin);
-
-    Serial.print("수축\n");
-
     sv1.Contraction(unit_angle);
     delay(150);
     sv1.detach();
 
     sv2.attach(sv2.pin);
-
     sv2.Contraction(unit_angle);
     delay(150);
     sv2.detach();
+
     sv3.attach(sv3.pin);
     sv3.Contraction(unit_angle);
     delay(150);
-
-
     sv3.detach();
-    Leg_PumpOn();
   }
 
   void Neutral() {  // 중립 상태
@@ -261,6 +237,7 @@ public:
     sv2.write(60);
     sv3.write(60);
     delay(700);
+
     sv1.detach();
     sv2.detach();
     sv3.detach();
@@ -269,20 +246,18 @@ public:
 
 
   void Forward() {  //예시
-    while (true) {
       Bending12();
       //Neutral();
-      delay(100);
+      delay(1500);
       Bending23();
       // Neutral();
-      delay(100);
+      delay(1500);
       Bending31();
       // Neutral();
-      delay(100);
-    }
+      delay(1500);
   }
-  void Backward() {  //예시
 
+  void Backward() {  //예시
     Bending31();
     // Neutral();
     delay(1500);
@@ -291,6 +266,15 @@ public:
     Bending12();
     // Neutral();
     delay(1500);
+    Bending23();
+    delay(1500);
+  }
+
+  void TestOff() {
+    pump1.TestOff();
+    pump2.TestOff();
+    pump3.TestOff();
+    Serial.println("Pumps Shutdown");
   }
 };
 
@@ -299,11 +283,11 @@ public:
   Leg FL, FR, RL, RR;  // Front Left, Front Right, Rear Left, Rear Right
   int tmp;
 
-  Robot(const int S_PIN[12], const int P_PIN[12], const int speedarray[12])
-    : FL(S_PIN[0], S_PIN[1], S_PIN[2], P_PIN[0], P_PIN[1], P_PIN[2], speedarray[0],speedarray[1],speedarray[2]),
-      FR(S_PIN[3], S_PIN[4], S_PIN[5], P_PIN[3], P_PIN[4], P_PIN[5],speedarray[3],speedarray[4],speedarray[5]),
-      RL(S_PIN[6], S_PIN[7], S_PIN[8], P_PIN[6], P_PIN[7], P_PIN[8],speedarray[6],speedarray[7],speedarray[8]),
-      RR(S_PIN[9], S_PIN[10], S_PIN[11], P_PIN[9], P_PIN[10], P_PIN[11],speedarray[9],speedarray[10],speedarray[11]) {}
+  Robot(const int S_PIN[12], const int P_PIN[12])
+    : FL(S_PIN[0], S_PIN[1], S_PIN[2], P_PIN[0], P_PIN[1], P_PIN[2]),
+      FR(S_PIN[3], S_PIN[4], S_PIN[5], P_PIN[3], P_PIN[4], P_PIN[5]),
+      RL(S_PIN[6], S_PIN[7], S_PIN[8], P_PIN[6], P_PIN[7], P_PIN[8]),
+      RR(S_PIN[9], S_PIN[10], S_PIN[11], P_PIN[9], P_PIN[10], P_PIN[11]) {}
 
   void Initialize() {
     FL.Initialize();
@@ -313,7 +297,6 @@ public:
   }
 
   void Forward() {  // 예시
-    while (true) {
       FL.Forward();
       FR.Forward();
       RL.Neutral();
@@ -324,7 +307,6 @@ public:
       RL.Forward();
       RR.Forward();
       delay(500);
-    }
   }
 
   void Backward() {
@@ -341,28 +323,31 @@ public:
 
 
 
-int s1 = A0;
-int s2 = A1;
-int s3 = A2;
+int s1 =24, s2 =25, s3 =26;
+int s4 =28, s5 =29, s6 =30;
+int s7 =32, s8 =33, s9 =34;
+// int s10 = , s11 = , s12 = ;
 
-int p1 = 3;
-int p2 = 6;
-int p3 = A5;
+int p1 =A0, p2 =A2, p3 =A4;
+int p4 =A6, p5 =A8, p6 =A10;
+int p7 =2, p8 =4, p9 =6;
+// int p10 =, p11 = , p12 = ;
 
-int spd1 = 254;
-int spd2 = 150;
-int spd3=254;
-Leg l1(s1, s2, s3, p1, p2, p3,spd1,spd2,spd3);
+
+Leg l1(s1, s2, s3, p1, p2, p3);
+Leg l2(s4, s5, s6, p4, p5, p6);
+Leg l3(s7, s8, s9, p7, p8, p9);
 
 
 void setup() {
   Serial.begin(9600);
   l1.Initialize();
+  l2.Initialize();
+  l3.Initialize();
 }
 
 void loop() {
   String input = Serial.readStringUntil('\n');  // character 변수, 알파벳 혹은 숫자 하나만 인식
-  // Serial.println(current_servo);
   input.trim();
 
   if (input == "p") {
@@ -386,21 +371,25 @@ void loop() {
 
   if (input == "a") {
     l1.Extension();
+    l2.Extension();
+    l3.Extension();
   }
 
   if (input == "d") {
     l1.Contraction();
+    l2.Contraction();
+    l3.Contraction();
   }
 
-  if (input == "m") {
+  if (input == "s") {
     l1.Neutral();
+    l2.Neutral();
+    l3.Neutral();
   }
-
 
   if (input == "12") {
     l1.Bending12();
   }
-
   if (input == "23") {
     l1.Bending23();
   }
@@ -408,10 +397,54 @@ void loop() {
     l1.Bending31();
   }
 
+
   if (input == "Forward") {
-    l1.Forward();
+    while(true) {
+      l1.Forward();
+      delay(1500);
+      l2.Forward();
+      delay(1500);
+      l3.Forward();
+      delay(1500);
+
+      if (Serial.available() > 0) {
+        String innerCommand = Serial.readStringUntil('\n');
+        innerCommand.trim();
+
+        if (innerCommand == "Stop") {
+          Serial.println("break");
+          break;
+        }
+      }
+    }
   }
+
+
   if (input == "Backward") {
-    l1.Backward();
+    while(true) {
+      l1.Backward();
+      delay(1500);
+      l2.Backward();
+      delay(1500);
+      l3.Backward();
+      delay(1500);
+
+      if (Serial.available() > 0) {
+        String innerCommand = Serial.readStringUntil('\n');
+        innerCommand.trim();
+
+        if (innerCommand == "Stop") {
+          Serial.println("break");
+          break;
+        }
+      }
+    }
+  }
+
+
+  if (input == "Off") { // 펌프 급하게 끌 때
+    l1.TestOff();
+    l2.TestOff();
+    l3.TestOff();
   }
 }
