31ef4d072591f448b1fa7eee8a47624b5d0c34db
권준형 <165387277+joonh02@users.noreply.github.com>
2025-08-21 21:57:38 +0900

    A 걷는데 성공함

--- RobotControl0815/RobotLib/src/RobotLib.cpp
+++ RobotControl0815/RobotLib/src/RobotLib.cpp
@@ -86,6 +86,14 @@ void Leg::Leg_PumpOn() {
   pump3.PumpOn();
 }
 
+void Leg::Leg_PumpOff() {
+  pump1.PumpOff();
+  pump2.PumpOff();
+  pump3.PumpOff();
+}
+
+
+
 void Leg::Initialize() {
   sv1.Initialize();
   sv2.Initialize();
@@ -96,7 +104,7 @@ void Leg::Initialize() {
 }
 
 void Leg::Bending12() {
-  Leg_PumpOn();
+  Leg_PumpOff();
   Serial.println(F("Bend to 12"));
 
   sv3.attach(sv3.pin);
@@ -113,10 +121,12 @@ void Leg::Bending12() {
   sv2.Contraction(unit_angle);
   delay(150);
   sv2.detach();
+
+  Leg_PumpOn();
 }
 
 void Leg::Bending23() {
-  Leg_PumpOn();
+  Leg_PumpOff();
   Serial.println(F("Bend to 23"));
 
   sv1.attach(sv1.pin);
@@ -133,10 +143,11 @@ void Leg::Bending23() {
   sv3.Contraction(unit_angle);
   delay(150);
   sv3.detach();
+  Leg_PumpOn();
 }
 
 void Leg::Bending31() {
-  Leg_PumpOn();
+  Leg_PumpOff();
   Serial.println(F("Bend to 31"));
 
   sv2.attach(sv2.pin);
@@ -153,11 +164,16 @@ void Leg::Bending31() {
   sv1.Contraction(unit_angle);
   delay(150);
   sv1.detach();
+
+  Leg_PumpOn();
+
 }
 
 void Leg::Extension() {
-  Leg_PumpOn();
-  Serial.println(F("팽창"));
+  // Leg_PumpOn();
+  // Serial.println(F("팽창"));
+
+  Leg_PumpOff();
 
   sv1.attach(sv1.pin);
   sv1.Extension(unit_angle);
@@ -173,11 +189,16 @@ void Leg::Extension() {
   sv3.Extension(unit_angle);
   delay(150);
   sv3.detach();
+
+  Leg_PumpOn();
+  Serial.println(F("팽창"));
 }
 
 void Leg::Contraction() {
-  Leg_PumpOn();
-  Serial.println(F("수축"));
+  // Leg_PumpOn();
+  // Serial.println(F("수축"));
+
+  Leg_PumpOff();
 
   sv1.attach(sv1.pin);
   sv1.Contraction(unit_angle);
@@ -193,6 +214,9 @@ void Leg::Contraction() {
   sv3.Contraction(unit_angle);
   delay(150);
   sv3.detach();
+
+  Leg_PumpOn();
+  Serial.println(F("수축"));
 }
 
 void Leg::Neutral() {
@@ -212,59 +236,161 @@ void Leg::Neutral() {
 }
 
 void Leg::Forward() {
+  Contraction();
   Bending12();
+  delay(2000);
+
+  sv1.attach(sv1.pin);
+  sv1.Extension(unit_angle);
   delay(150);
-  Bending23();
+  sv1.detach();
+
+  sv2.attach(sv2.pin);
+  sv2.Extension(unit_angle);
   delay(150);
-  Bending31();
+  sv2.detach();
+
+  sv3.attach(sv3.pin);
+  sv3.Extension(unit_angle);
   delay(150);
+  sv3.detach();
+  
+  Leg_PumpOff();
+  delay(1000);
+
+  sv2.attach(sv3.pin);
+  sv2.Extension(unit_angle);
+  delay(150);
+  sv2.detach();
+
+  sv3.attach(sv3.pin);
+  sv3.Contraction(unit_angle);
+  delay(150);
+  sv3.detach();
+
+
+
+  pump2.PumpOn();
+  pump3.PumpOn();
+
+  delay(1600);
+
+
+  pump2.PumpOff();
+  pump3.PumpOff();
+  delay(2000);
+
+  sv1.attach(sv1.pin);
+  sv1.Contraction(unit_angle);
+  delay(150);
+  sv1.detach();
+
+  sv2.attach(sv2.pin);
+  sv2.Contraction(unit_angle);
+  delay(150);
+  sv2.detach();
+
+  sv3.attach(sv3.pin);
+  sv3.Contraction(unit_angle);
+  delay(150);
+  sv3.detach();
+
+  Leg_PumpOn();
+
+  delay(1000);
 }
 
 void Leg::Backward() {
   Bending31();
-  delay(1500);
+  delay(400);
+  Bending23();
+  delay(150);
   Bending12();
-  delay(1500);
+  delay(400);
+  Bending31();
+  delay(400);
+  Contraction();
+  delay(150);
 }
 
 // -------- Robot --------
-Robot::Robot(const int S_PIN[12], const int P_PIN[12], const int speedarray[12])
-    : FL(S_PIN[0], S_PIN[1], S_PIN[2], P_PIN[0], P_PIN[1], P_PIN[2], speedarray[0],speedarray[1],speedarray[2]),
-      FR(S_PIN[3], S_PIN[4], S_PIN[5], P_PIN[3], P_PIN[4], P_PIN[5],speedarray[3],speedarray[4],speedarray[5]),
-      RL(S_PIN[6], S_PIN[7], S_PIN[8], P_PIN[6], P_PIN[7], P_PIN[8],speedarray[6],speedarray[7],speedarray[8]),
-      RR(S_PIN[9], S_PIN[10], S_PIN[11], P_PIN[9], P_PIN[10], P_PIN[11],speedarray[9],speedarray[10],speedarray[11]) {}
+Robot::Robot(const int S_PIN[9], const int P_PIN[9], const int speedarray[9])
+    : A(S_PIN[0], S_PIN[1], S_PIN[2], P_PIN[0], P_PIN[1], P_PIN[2], speedarray[0],speedarray[1],speedarray[2]),
+      B(S_PIN[3], S_PIN[4], S_PIN[5], P_PIN[3], P_PIN[4], P_PIN[5],speedarray[3],speedarray[4],speedarray[5]),
+      C(S_PIN[6], S_PIN[7], S_PIN[8], P_PIN[6], P_PIN[7], P_PIN[8],speedarray[6],speedarray[7],speedarray[8]) {}
+
+      
 
 void Robot::Initialize() {
-  FL.Initialize();
-  FR.Initialize();
-  RL.Initialize();
-  RR.Initialize();
+  A.Initialize();
+  B.Initialize();
+  C.Initialize();
 }
 
-void Robot::FL_RR_Forward() {
-  FL.Bending12(); delay(1500);
-  RR.Bending12(); delay(1500);
-  FL.Bending23(); delay(1500);
-  RR.Bending23(); delay(1500);
-  FL.Bending31(); delay(1500);
-  RR.Bending31(); delay(1500);
+void Robot::AB_Forward() {
+
+  A.Bending12();
+  B.Bending31();
+  delay(200);
+  A.Bending23();
+  B.Bending23();
+  delay(200);
+  A.Bending31();
+  B.Bending12();
+  delay(200);
+  A.Contraction();
+  delay(150);
+
+
+    // A.Forward();
+    // B.Backward();
+    // A.Contraction();
+    // B.Contraction();
+    // delay(150);
+    
+    
+    // A.Forward();
+    // B.Backward();
+    // A.Contraction();
+    // B.Contraction();
+    
+
+    
+//    A.Bending31();
+//    delay(1500);
+//    B.Bending12();
+//    delay(1500);
+//    C.Bending31();
+//    delay(1500);
+//    
+//    A.Bending23();
+//    delay(1500);
+//    B.Bending23();
+//    delay(1500);
+//    C.Bending12();
+//    delay(1500);
+//    
+//    A.Bending12();
+//    delay(1500);
+//    B.Bending31();
+//    delay(1500);
+//    C.Bending31();
+//    delay(1500);
 }
 
-void Robot::FR_RL_Forward() {
-  FR.Bending12(); delay(1500);
-  RL.Bending12(); delay(1500);
-  FR.Bending23(); delay(1500);
-  RL.Bending23(); delay(1500);
-  FR.Bending31(); delay(1500);
-  RL.Bending31(); delay(1500);
-}
+//void Robot::FR_RL_Forward() {
+//
+//}
 
 void Robot::Forward()  {
-    Serial.println("Robot is going straight");
-    FL_RR_Forward();
-    FR_RL_Forward();
 }
 void Robot::Backward() { /* TODO */     }
 void Robot::TurnRight(){ /* TODO */ }
 void Robot::TurnLeft() { /* TODO */ }
+
+void Robot::Standing() {
+    A.Bending23();
+    B.Bending23();
+    C.Bending23();
+}
 
